64c79e8814be2ad1b8f47a500d7ced79d7e7a353
capitalistspz <keipitalists@proton.me>
2026-07-12 11:24:30 +0100

    Reserve Bind

---
+++ Patches/Tools/ReserveBind.cs
@@ -0,0 +1,98 @@
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using JetBrains.Annotations;
+using Rebalance.FSMActions;
+using Rebalance.FsmUtils;
+using Silksong.FsmUtil;
+
+namespace Rebalance.Patches.tools;
+
+public static class ReserveBind
+{
+    private const float ChargeRate = 5.0f;
+    private const string StartHealthVarName = "Rebalance - Starting Health";
+
+    [FsmMutator("Hero_Hornet(Clone)", "Bind")]
+    [UsedImplicitly]
+    public static void ReduceChargeTimeAndMakeRestorable(Fsm fsm)
+    {
+        #region "Get States"
+        var canBindState = fsm.GetState("Can Bind?");
+        if (canBindState == null)
+        {
+            RebalancePlugin.Logger.LogError("Failed to find Can Bind? state");
+            return;
+        }
+
+        var endBindState = fsm.GetState("End Bind");
+        if (endBindState == null)
+        {
+            RebalancePlugin.Logger.LogError("Failed to find Do Bind state");
+            return;
+        }
+
+        var reserveBindChargeState = fsm.GetState("Reserve Bind Charge");
+        if (reserveBindChargeState == null)
+        {
+            RebalancePlugin.Logger.LogError("Failed to find Reserve Bind Charge state");
+            return;
+        }
+
+        var reserveBindBurst = fsm.GetState("Reserve Bind Burst");
+        if (reserveBindBurst == null)
+        {
+            RebalancePlugin.Logger.LogError("Failed to find Reserve Bind Burst state");
+            return;
+        }
+        #endregion
+        #region "Reduce Reserve Bind Delay"    
+        reserveBindChargeState.ReplaceFirstActionOfType<Tk2dPlayAnimationWithEvents>(
+            new Tk2dPlayAnimationWithEventsAtRate
+            {
+                gameObject = new FsmOwnerDefault(),
+                clipName = fsm.GetFsmString("Play Anim"),
+                animationCompleteEvent = FsmEvent.Finished,
+                playRate = ChargeRate
+            });
+        
+        reserveBindBurst.RemoveFirstActionOfType<FreezeMoment>(); 
+        
+        reserveBindBurst.ReplaceFirstActionOfType<Tk2dPlayAnimationWithEvents>(
+            new Tk2dPlayAnimationWithEventsAtRate
+            {
+                gameObject = new FsmOwnerDefault(),
+                clipName = fsm.GetFsmString("Play Anim"),
+                animationCompleteEvent = FsmEvent.Finished,
+                playRate = ChargeRate
+            });
+        #endregion
+        #region "Restore on Bind"
+        var startingHealthVar = fsm.AddIntVariable(StartHealthVarName);
+        canBindState.InsertAction(0, new GetPlayerDataInt
+        {
+            intName = "health",
+            storeValue = startingHealthVar
+        });
+        
+        endBindState.AddMethod(action =>
+        {
+            if (startingHealthVar.IsNone)
+                return;
+            if (startingHealthVar.Value < PlayerData.instance.CurrentMaxHealth)
+                return;
+            
+            // Don't want reserve bind to produce another reserve bind
+            var usedReserveBind = action.fsm.GetFsmBool("Used Reserve Bind");
+            if (!NamedVariable.IsNullOrNone(usedReserveBind) && usedReserveBind.Value)
+                return;
+            var reserveBindTool = ToolItemManager.GetToolByName("Reserve Bind");
+            if (reserveBindTool != null)
+            {
+                reserveBindTool.ReloadSingle();
+                // To make the reserve bind display in the HUD
+                EventRegister.SendEvent(EventRegisterEvents.EquipsChangedEvent);
+            }
+        });
+        #endregion
+    }
+}
